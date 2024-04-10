e6d1ccdd81eae26bb88345133bbdf6a063e8bfd3
sirpaoFR <pierrealo57@gmail.com>
2024-04-10 14:53:57 +0200

    fais animation de saut et des colliders

--- GravityClip/Assets/Script/Mouvement.cs
+++ GravityClip/Assets/Script/Mouvement.cs
@@ -1,19 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))] // Assurez-vous qu'un Rigidbody est attaché au GameObject
+[RequireComponent(typeof(Rigidbody2D))] // Assurez-vous qu'un Rigidbody2D est attaché au GameObject
 public class DeplacementPersonnage : MonoBehaviour
 {
     public float vitesseDeplacement = 5f; // Vitesse de déplacement du personnage
-    private Rigidbody rb; // Référence au composant Rigidbody
+    public float forceSaut = 10f; // Force du saut
+    private Rigidbody2D rb; // Référence au composant Rigidbody2D
     private bool vaADroite = true; // Variable pour suivre la direction du personnage
 
     // Méthode appelée lorsque le script est initialisé
     void Awake()
     {
-        rb = GetComponent<Rigidbody>(); // Obtenez le Rigidbody attaché au GameObject
-        rb.freezeRotation = true; // Empêcher la rotation du Rigidbody
+        rb = GetComponent<Rigidbody2D>(); // Obtenez le Rigidbody2D attaché au GameObject
+        rb.freezeRotation = true; // Empêcher la rotation du Rigidbody2D
     }
 
     // Méthode appelée à chaque frame
@@ -21,7 +20,7 @@ public class DeplacementPersonnage : MonoBehaviour
     {
         // Déplacement horizontal du personnage
         float deplacementHorizontal = Input.GetAxis("Horizontal") * vitesseDeplacement * Time.deltaTime;
-        Vector3 deplacement = new Vector3(deplacementHorizontal, 0, 0);
+        Vector2 deplacement = new Vector2(deplacementHorizontal, 0);
         transform.Translate(deplacement);
 
         // Inversion de la direction du personnage
@@ -32,6 +31,12 @@ public class DeplacementPersonnage : MonoBehaviour
         else if (deplacementHorizontal < 0 && vaADroite)
         {
             RetournerPersonnage();
+        }
+
+        // Gestion du saut
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Sauter();
         }
     }
 
@@ -44,10 +49,9 @@ public class DeplacementPersonnage : MonoBehaviour
         transform.localScale = nouvelleEchelle;
     }
 
-    // Méthode pour gérer la gravité
-    void FixedUpdate()
+    // Méthode pour gérer le saut
+    void Sauter()
     {
-        rb.AddForce(Vector3.down * 9.81f); // Appliquer une force de gravité constante
+        rb.AddForce(Vector2.up * forceSaut, ForceMode2D.Impulse); // Appliquer une force de saut
     }
 }
-
