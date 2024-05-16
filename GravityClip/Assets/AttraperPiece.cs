999384e2852a3b9b172ae1eea9b554f175feaacc
sirpaoFR <pierrealo57@gmail.com>
2024-05-16 20:05:15 +0200

    système de pièce fini

---
+++ GravityClip/Assets/AttraperPiece.cs
@@ -0,0 +1,13 @@
+
+using UnityEngine;
+
+public class AttraperPiece : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
