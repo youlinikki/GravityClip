0902f53e897e1c0bdcb81da4d5ea77a4297cba1d
youlinikki <pourgenimyouli@gmail.com>
2024-05-18 13:45:06 +0200

    camera update

--- GravityClip/Assets/Script/camera.cs
+++ GravityClip/Assets/Script/camera.cs
@@ -9,6 +9,10 @@ public class CameraFollow : MonoBehaviour
     public float maxZoom = 15.0f; // Zoom maximal
     public float smoothSpeed = 0.125f; // Vitesse de d�placement de la cam�ra
 
+    public int minX;
+
+    public int maxX;
+
     GameObject fusionControl;
     void Start(){
 
@@ -16,30 +20,36 @@ public class CameraFollow : MonoBehaviour
     }
     void LateUpdate()
     {
+    
+
+
+        
         if (fusionControl.GetComponent<fusion>().etat==0 ||fusionControl.GetComponent<fusion>().etat==1 )
         {
             Vector3 milieu = (joueur1.position + joueur2.position) / 2f;
             float distance = Vector3.Distance(joueur1.position, joueur2.position);
             float newZoom = Mathf.Lerp(maxZoom, minZoom, distance / maxZoom);
-
-            transform.position = Vector3.Lerp(transform.position, new Vector3(milieu.x, transform.position.y, -newZoom), smoothSpeed * Time.deltaTime);
+            if(new Vector3(milieu.x, transform.position.y, -newZoom).x > minX && new Vector3(milieu.x, transform.position.y, -newZoom).x < maxX){
+            transform.position = Vector3.Lerp(transform.position, new Vector3(milieu.x, transform.position.y, -newZoom), smoothSpeed * Time.deltaTime);}
 
         }else if(fusionControl.GetComponent<fusion>().etat==4 || fusionControl.GetComponent<fusion>().etat==3) {
              Vector3 milieu = joueur2.position;
             float distance = Vector3.Distance(joueur1.position, joueur2.position);
             float newZoom = Mathf.Lerp(maxZoom, minZoom, distance / maxZoom);
-
+            if(new Vector3(milieu.x, transform.position.y, -newZoom).x > minX && new Vector3(milieu.x, transform.position.y, -newZoom).x < maxX){
             transform.position = Vector3.Lerp(transform.position, new Vector3(milieu.x, transform.position.y, -newZoom), smoothSpeed * Time.deltaTime);
-
+            }
 
         }else if(fusionControl.GetComponent<fusion>().etat==6 || fusionControl.GetComponent<fusion>().etat==5) {
 
              Vector3 milieu = joueur1.position;
             float distance = Vector3.Distance(joueur1.position, joueur2.position);
             float newZoom = Mathf.Lerp(maxZoom, minZoom, distance / maxZoom);
-
+            if(new Vector3(milieu.x, transform.position.y, -newZoom).x > minX && new Vector3(milieu.x, transform.position.y, -newZoom).x < maxX){
             transform.position = Vector3.Lerp(transform.position, new Vector3(milieu.x, transform.position.y, -newZoom), smoothSpeed * Time.deltaTime);
-            
+            }
         }
+
     }
+
 }
